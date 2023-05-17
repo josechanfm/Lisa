41fa7b232672bb22a8ec57539ebf62f0b6695022
hipawudi <abc95175346@hotmail.com>
2023-05-17 18:17:06 +0800

    feat: hubis link

--- layouts/default/footer.vue
+++ layouts/default/footer.vue
@@ -4,11 +4,13 @@
       <div class="flex flex-wrap justify-between items-center -mx-2">
         <div class="w-full lg:w-2/6 px-2">
           <p class="hidden lg:block text-sm text-gray-500">
-            All rights reserved, Copyright © {{ new Date().getFullYear() }} Anthony Loi
+            All rights reserved, Copyright © {{ new Date().getFullYear() }}
+            <a class="text-blue-600" href="https://hubis.ltd">Hubis</a>
           </p>
         </div>
         <p class="mt-6 lg:hidden text-sm text-gray-500">
-          All rights reserved, Copyright © {{ new Date().getFullYear() }} Anthony Loi
+          All rights reserved, Copyright © {{ new Date().getFullYear() }}
+          <a class="text-blue-600" href="https://hubis.ltd">Hubis</a>
         </p>
       </div>
     </div>
